056f582f60414c95a9aeebb8fd8d94adee141b7c
bmuhamood <bbosa2009@gmail.com>
2021-09-01 19:12:32 +0400

    Final commit

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,21 +1,24 @@
 class Recipe
-    attr_accessor :recipe_name, :ingredients, :method_steps  
+    attr_accessor :recipe_name, :ingredients
     
-    def initialize(name, ingredients, method_steps)
-        @recipe_name = name
+    def initialize(name, ingredients)
+        @recipe_name = recipe_name
         @ingredients = ingredients
-        @method_steps = method_steps
     end
 
-    def recipe_initializers
+    def method(pancake_recipe, soup_recipe)
+        @pancake_recipe = 'Mix the ingredients', 'Cook them in a pan'
+        @soup_recipe = 'Mix miso paste into boiling water', 'Add tofu and serve'
+    end
+
+    def recipe_initializers()
         puts @recipe_name
         puts @ingredients
-        puts @method_steps
     end
 end
 
-pancake_recipe = Recipe.new('Pancake', 'Store-bought pancake mix', 'Water')
-soup_recipe = Recipe.new('Miso Soup', 'Tofu', 'White miso paste')
+pancake_recipe = Recipe.new(pancake_recipe, 'Pancake', 'Store-bought pancake mix', 'Water')
+soup_recipe = Recipe.new(soup_recipe, 'Miso Soup', 'Tofu', 'White miso paste')
 
 pancake_recipe.recipe_initializers
 soup_recipe.recipe_initializers
